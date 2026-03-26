33b0837f1f513f930a72de4959f353df6277b01c
Ricardo Alberto González Cedeno <169106488+RickGZ007@users.noreply.github.com>
2026-03-26 16:47:03 -0300

    cria interface de calculadora com componentes e props em React
    Criação da interface gráfica de uma calculadora em React utilizando
    componentes reutilizáveis, props e estilização com CSS.
    A aplicação contém os componentes Button, Display e Calculator.
    Não há funcionalidade nos botões, apenas layout conforme solicitado.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
 import './App.css';
-
-const Nome = (props) => {
-  return (
-    <div className="estilo">
-     {props.texto}
-    </div>
-  )
-}
- 
+import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div>
-      <Nome texto="Rick" />
-      <Nome texto="Morty" />
-      <Nome texto="Summer" />
-      <Nome texto="Beth" />
-      <Nome texto="Jerry" />
-      <Nome texto="Birdperson" />
-      <Nome texto="Squanchy" />
-      
+    <div className="App">
+      <Calculator />
     </div>
   );
 }
